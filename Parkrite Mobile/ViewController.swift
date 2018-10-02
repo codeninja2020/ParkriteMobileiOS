ac6cf80d7a22dea106068351d77278c46ec40907
Progress Moyo <progressmoyo@progresss-macbook-pro.local>
2018-10-02 11:24:45 +0200

    Initial Commit

---
+++ Parkrite Mobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parkrite Mobile
+//
+//  Created by Progress Moyo on 2/10/2018.
+//  Copyright © 2018 Parkrite Zambia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
